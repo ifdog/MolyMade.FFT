30d8ab61fc02440325dc89b45551ea2c3291b4f6
ifdog <zouyuanpeng@163.com>
2016-10-10 22:46:29 +0800

    md,zz

---
+++ MolyMade.FFT/MolyMade.FFT/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MolyMade.FFT
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void button_Click(object sender, RoutedEventArgs e)
+		{
+			Transformer t = new Transformer();
+			var x = t.test(new double[] {1, 2, 3, 4, 5, 6});
+		}
+	}
+}
---
+++ MolyMade.FFT/MolyMade.FFT/Transformer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MolyMade.FFT
+{
+	public class Transformer
+	{
+
+		public double[,] Fft2D(double[,] doubles)
+		{
+			
+			var rows = doubles.Select(d =>
+			{
+				alglib.complex[] rowComplexs;
+				alglib.fftr1d(d,out rowComplexs);
+				return rowComplexs;
+			}).ToArray();
+
+		}
+
+		private alglib.complex[][] zz(alglib.complex[][] old)
+		{
+			int x = old.GetUpperBound(0);
+			int y = old.GetUpperBound(1);
+			var neu = new alglib.complex[y+1][x+1];
+			for (int i = 0; i <= y; i++)
+			{
+				for (int j = 0; j <= x; j++)
+				{
+					neu[i, j] = old[j, i];
+				}
+			}
+			return neu;
+		}
+	}
+}
